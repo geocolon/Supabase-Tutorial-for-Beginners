2a14a4c6724035f87d559d6eed42b087abcd78b7
George Colon <geocolon@gmail.com>
2025-07-24 12:15:25 -0700

    updated index.css

--- src/components/SmoothieCard.js
+++ src/components/SmoothieCard.js
@@ -2,9 +2,11 @@ const SmoothieCard = ({ smoothie }) => {
   return (
     <div className="smoothie-card">
       <h2>{smoothie.title}</h2>
-      <p>{smoothie.ingredients}</p>
+      {/* <p>{smoothie.ingredients}</p> */}
       <h3>{smoothie.title}</h3>
-      <p>{smoothie.method}</p>
+      <p
+      className="single-line-ellipsis"
+      >{smoothie.method}</p>
       <p>Created at: {new Date(smoothie.created_at).toLocaleDateString()}</p>
       <div className="rating">{smoothie.rating}</div>
     </div>
